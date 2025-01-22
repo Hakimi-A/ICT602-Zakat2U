b3bf81cfa48f0a44534e218a18d6571067d81f63
Hakimi Anuar <mohdhaikalhakimi.mohdanuar@gmail.com>
2025-01-23 02:33:17 +0800

    Final Commit

--- app/src/main/java/com/example/mockzakatgold/MainActivity.java
+++ app/src/main/java/com/example/mockzakatgold/MainActivity.java
@@ -46,7 +46,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         AppToolbar = findViewById(R.id.tbAppTitle);
         setSupportActionBar(AppToolbar);
-        getSupportActionBar().setTitle("Zakat for Gold Calculator");
     }
 
     @Override
